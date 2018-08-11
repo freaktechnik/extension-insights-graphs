9ccd45050a05a458e7b6bdf5cafbe30aaf3fdb99
Martin Giger <martin@humanoids.be>
2018-08-11 17:30:48 +0200

    Switch to CSV parsing only

--- index.js
+++ index.js
@@ -1,125 +1,56 @@
-const HEX = 16,
-    clientId = '1x59tb2nluf1dgnx5aqazviiibft56',
-    ignoredCols = [
+const ignoredCols = [
         'Date',
         'Extension Name',
         'Extension Client ID'
-    ];
+    ],
+    reader = new FileReader();
 
-if(location.hash.length > 1 || localStorage.getItem("token")) {
-    let token = localStorage.getItem('token');
-    if(localStorage.getItem("state") && location.hash.length > 1) {
-        const params = new URLSearchParams(location.hash.substr(1));
-        if(params.get('state') === localStorage.getItem('state') && params.get('scope') === 'analytics:read:extensions') {
-            token = params.get('access_token');
-            localStorage.setItem("token", token);
-            localStorage.removeItem('state');
-        }
-        else {
-            throw new Error("Couldn't authorize");
+reader.addEventListener("load", () => {
+    const data = d3.csvParse(reader.result);
+    const statSelect = document.getElementById("stat");
+    for(const child of statSelect.children) {
+        child.remove();
+    }
+    for(const column of data.columns) {
+        if(!ignoredCols.includes(column)) {
+            const opt = new Option(column);
+            statSelect.append(opt);
         }
     }
-    else if(!token) {
-        throw new Error("Invalid application state");
-    }
+    statSelect.addEventListener("change", () => {
+        const svg = d3.select("svg"),
+            margin = {top: 20, right: 20, bottom: 30, left: 50},
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom,
+            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+            parseTime = d3.timeParse("%d-%b-%y"),
+            x = d3.scaleTime().rangeRound([0, width]),
+            y = d3.scaleLinear().rangeRound([height, 0]),
+            line = d3.line().x((d) => parseTime(d.Date)).y((d) => d[statSelect.value]);
+        x.domain(d3.extend(data, (d) => parseTime(d.Date)));
+        y.domain(d3.extend(data, (d) => d[statSelect.value]));
+        g.append("g")
+                .call(d3.axisLeft(y))
+            .append("text")
+                .attr("fill", "#000")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .attr("text-anchor", "end")
+                .text(statSelect.value);
 
-    document.getElementById("login").hidden = true;
-    document.getElementById("graphs").hidden = false;
-
-    document.getElementById("logout").addEventListener("click", () => {
-        localStorage.removeItem("token");
-        location.hash = '';
-        location.reload();
+        g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
     });
+});
 
-    const extensionSelect = document.getElementById("extension");
-    extensionSelect.addEventListener("change", () => {
-        if(extensionSelect.value) {
-            //BAD IDEA
-            fetch("https://crossorigin.me/" + extensionSelect.value).then((response) => {
-                if(response.ok && response.status === 200) {
-                    return response.text();
-                }
-                throw new Error("Could not load extension insights");
-            }).then((csv) => {
-                const data = d3.csvParse(csv);
-                const statSelect = document.getElementById("stat");
-                for(const child of statSelect.children) {
-                    child.remove();
-                }
-                for(const column of data.columns) {
-                    if(!ignoredCols.includes(column)) {
-                        const opt = new Option(column);
-                        statSelect.append(opt);
-                    }
-                }
-                statSelect.addEventListener("change", () => {
-                    const svg = d3.select("svg"),
-                        margin = {top: 20, right: 20, bottom: 30, left: 50},
-                        width = +svg.attr("width") - margin.left - margin.right,
-                        height = +svg.attr("height") - margin.top - margin.bottom,
-                        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-                        parseTime = d3.timeParse("%d-%b-%y"),
-                        x = d3.scaleTime().rangeRound([0, width]),
-                        y = d3.scaleLinear().rangeRound([height, 0]),
-                        line = d3.line().x((d) => parseTime(d.Date)).y((d) => d[statSelect.value]);
-                    x.domain(d3.extend(data, (d) => parseTime(d.Date)));
-                    y.domain(d3.extend(data, (d) => d[statSelect.value]));
-                    g.append("g")
-                            .call(d3.axisLeft(y))
-                        .append("text")
-                            .attr("fill", "#000")
-                            .attr("transform", "rotate(-90)")
-                            .attr("y", 6)
-                            .attr("dy", "0.71em")
-                            .attr("text-anchor", "end")
-                            .text(statSelect.value);
-
-                    g.append("path")
-                        .datum(data)
-                        .attr("fill", "none")
-                        .attr("stroke", "steelblue")
-                        .attr("stroke-linejoin", "round")
-                        .attr("stroke-linecap", "round")
-                        .attr("stroke-width", 1.5)
-                        .attr("d", line);
-                });
-            }).catch(console.error);
-        }
-    });
-
-    fetch(`https://api.twitch.tv/helix/analytics/extensions?type=overview_v2`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }).then((response) => {
-        if(response.ok && response.status === 200) {
-            return response.json();
-        }
-        throw new Error(`HTTP error ${response.statusText}`);
-    }).then((json) => {
-        //TODO pagination
-        for(const extension of json.data) {
-            if(extension.type === "overview_v2") {
-                const opt = new Option(extension.extension_id, extension.URL);
-                extensionSelect.append(opt);
-            }
-        }
-        // URLs are no longer valid after 1 minute.
-        //TODO instead just request a new url when the extension is selected.
-        setTimeout(() => {
-            for(const child of extensionSelect.children) {
-                if(!child.selected && child.value) {
-                    child.remove();
-                }
-            }
-        }, 60000);
-    }).catch(console.error);
-}
-else {
-    const state = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(HEX);
-    localStorage.setItem("state", state);
-    document.querySelector("#login button").addEventListener("click", () => {
-        window.location = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${location.origin}${location.pathname}${location.search}&response_type=token&scope=analytics:read:extensions&state=${state}`;
-    });
-}
+doucment.getElementById("source").addEventListener("input", (e) => {
+    const file = e.target.files[0];
+    reader.readAsText(file);
+});
